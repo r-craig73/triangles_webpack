3a3e802ef7178dd52a4d35f3450a8273cc2efeca
Jared Clemmensen and Ron Craig <jared+ron@>
2018-03-13 17:20:03 -0700

    latest verion, no sudoku here

--- src/main.js
+++ src/main.js
@@ -1,21 +1,21 @@
-//import { tbd } from './sample-project';
 import './styles.css';
 import $ from 'jquery';
-// import 'boostrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Triangle} from './triangle';
+// import {Triangle} from './triangle';
 
 $(document).ready(function() {
+<<<<<<< HEAD
+  console.log("test");
+  $("#submit").submit(function(event) {
+    var storeRow = [];
+=======
   $("form#triangle").submit(function(event) {
     var side1 = parseInt($("input#length1").val());
     var side2 = parseInt($("input#length2").val());
     var side3 = parseInt($("input#length3").val());
 
     var triangle = new Triangle(side1, side2, side3);
-    if (triangle.isTriangle() === false){
-      $(".result1").text("This is not a triangle :-(");
-    }
-    else if (triangle.equilateral()) {
+    if (triangle.equilateral()) {
       $(".result1").text("This is an equilateral triangle");
     }
     else if(triangle.isosceles()) {
@@ -24,8 +24,15 @@ $(document).ready(function() {
     else if (triangle.scalene()) {
       $(".result1").text("This is an scalene triangle");
     }
+    else if (!triangle.isTriangle){
+      $(".result1").text("This is not a triangle :-(");
+    }
+>>>>>>> parent of e3b54ea... fix isTriangle interface bug
 
+    storeRow = document.getElementsByClassName("cell").val();
+    console.log(storeRow);
     event.preventDefault();
 
   });
+
 });
